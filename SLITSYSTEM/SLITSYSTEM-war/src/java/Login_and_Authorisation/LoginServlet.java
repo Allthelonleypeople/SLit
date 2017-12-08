f906d04350bdf709ac7ce99487395ed5b2de09e7
RasmusRS <rasmus16@student.uia.no>
2017-12-08 09:58:35 +0100

    Add files via upload

---
+++ SLITSYSTEM/SLITSYSTEM-war/src/java/Login_and_Authorisation/LoginServlet.java
@@ -0,0 +1,71 @@
+package Login_and_Authorisation;
+
+
+import Management.UserManagerLocal;
+import users.Bruker;
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import javax.servlet.http.HttpSession;
+
+/**
+ * @author Rasmus
+ */
+@WebServlet ("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+
+    @EJB
+    UserManagerLocal manager;
+
+    private void login(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        request.setCharacterEncoding("UTF-8");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password"); 
+        
+        // Prøver å finne brukernavn i database
+        //Måler brukernavn og string passord opp mot passord i database
+        try {
+            Bruker b = manager.getUser(email.toLowerCase());
+        if (b.getPassword().equals(password)){
+            //hvis brukernavn og passord er riktig starter den en ny session. 
+            HttpSession session = request.getSession();
+            session.setAttribute("loginUser", b);
+            
+            //sjekker brukertype om det er admin, lærer eller student
+            if (b.getUserType().equals("admin")) {                   
+            response.sendRedirect("/SLITSYSTEM-war/AdminMenu");
+            }
+            else if (b.getUserType().equals("teacher")) {                   
+            response.sendRedirect("/SLITSYSTEM-war/TeacherMenu");
+            } 
+            else {
+            response.sendRedirect("/SLITSYSTEM-war/StudentMenu");
+            }
+        }        
+        else{
+                response.sendRedirect("/SLITSYSTEM-war/loginfeil.html");       
+            }            
+        }
+        catch (NullPointerException nullp) {
+            System.err.println("NullPointerException: " + nullp.getMessage());
+            response.sendRedirect("/SLITSYSTEM-war/loginfeil.html");
+    }
+    }
+
+    @Override
+   protected void doGet(HttpServletRequest request, HttpServletResponse response)
+           throws ServletException, IOException {
+        login(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException{
+        login(request, response);
+    }
+}
+
---
+++ SLITSYSTEM/SLITSYSTEM-war/src/java/Login_and_Authorisation/NewBrukerServlet.java
@@ -0,0 +1,59 @@
+package Login_and_Authorisation;
+
+import Management.UserManagerLocal;
+        import users.Bruker;
+        import javax.ejb.EJB;
+        import javax.servlet.ServletException;
+        import javax.servlet.annotation.WebServlet;
+        import javax.servlet.http.HttpServlet;
+        import javax.servlet.http.HttpServletRequest;
+        import javax.servlet.http.HttpServletResponse;
+        import java.io.IOException;
+        import java.io.PrintWriter;
+
+
+@WebServlet ("/NewBrukerServlet")
+
+
+public class NewBrukerServlet extends HttpServlet {
+
+    @EJB
+    UserManagerLocal manager;
+    // etode for å legge til en ny bruker. 
+    private void newBruker (HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        request.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+        String email = request.getParameter("email");
+        String password = request.getParameter("passWord");
+        String fName = request.getParameter("firstName");
+        String lName = request.getParameter("lastName");
+        String userType = request.getParameter("userType"); 
+        
+
+        Bruker b = new Bruker(email.toLowerCase(), password, fName, lName, userType);
+        if(manager.saveUser(b) == true){
+            out.print("Din bruker har blitt opprettet!  ");
+            out.print("Du vil nå bli videresendt til innloggingen   ");            
+            response.sendRedirect("/SLITSYSTEM-war/Registrer.html");
+        } else if (manager.saveUser(b) == false){
+            out.print("Din bruker kunne ikke bli opprettet, vennligst prøv igjen ");
+        }
+        
+      
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        newBruker(request, response);
+    }
+
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        newBruker(request, response);
+    }
+}
